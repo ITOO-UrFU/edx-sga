e17875c013a09ed22cdb2f73244c63e315ac6d2d
RaccoonBoyG <alexofficialkek@gmail.com>
2020-05-20 10:37:42 +0500

    staff_score add edit fields

--- edx_sga/sga.py
+++ edx_sga/sga.py
@@ -75,7 +75,7 @@ class StaffGradedAssignmentXBlock(StudioEditableXBlockMixin, ShowAnswerXBlockMix
     has_score = True
     icon_class = 'problem'
     STUDENT_FILEUPLOAD_MAX_SIZE = 4 * 1000 * 1000  # 4 MB
-    editable_fields = ('display_name', 'points', 'weight', 'showanswer', 'solution')
+    editable_fields = ('display_name', 'staff_score', 'points', 'weight', 'showanswer', 'solution')
 
     display_name = String(
         display_name=_("Display Name"),
